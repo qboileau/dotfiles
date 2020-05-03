bca9759dcfe4488438737423cfc64bfacbf8b0d5
qboileau <quentin.boileau@gmail.com>
2020-05-03 17:45:45 +0200

    add wip on rofi monitor script

---
+++ home/bin/rofi-monitors.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+all_output_list=(`xrandr -q | grep connected | awk '{print $1}'`)
+
+home_options() {
+
+}
+
+if [ x"$@" = x"quit" ]
+then
+    exit 0
+fi
+
+if [ "$@" ]
+then
+    # Override the previously set prompt.
+    echo -en "\x00prompt\x1fChange prompt\n"
+    for a in {1..10}
+    do
+        echo "$a"
+    done
+    echo "quit"
+else
+    echo -en "\x00prompt\x1ftesting\n"
+    echo -en "\0urgent\x1f0,2\n"
+    echo -en "\0active\x1f1\n"
+    echo -en "\0markup-rows\x1ftrue\n"
+    echo -en "\0message\x1fSpecial <b>bold</b>message\n"
+
+    echo -en "aap\0icon\x1ffolder\n"
+    echo "noot"
+    echo "mies"
+    echo "testing"
+    echo "<b>Bold</b>"
+    echo "quit"
+fi
